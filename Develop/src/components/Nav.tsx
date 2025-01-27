3d1198ecd98242e3f9a9c422a6b287ae5a423a5a
nkamalu620 <nkamalu96@gmail.com>
2025-01-26 20:17:34 -0500

    added code to nav.tsx and candidtate.interface.tsx

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,7 +1,27 @@
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/resume">Resume</Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
   )
 };
 
