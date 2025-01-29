59d1066e7f92c3bf2e9a583d04b1b8eddd1d0819
nkamalu620 <nkamalu96@gmail.com>
2025-01-28 23:41:55 -0500

    added code

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,6 @@
+// import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
@@ -8,17 +11,9 @@ const Nav = () => {
                     <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/about">About</Link>
+                    <Link to="/potential candidates">Potential Candidates</Link>
                 </li>
-                <li>
-                    <Link to="/portfolio">Portfolio</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/resume">Resume</Link>
-                </li>
+           
             </ul>
         </nav>
     </div>
